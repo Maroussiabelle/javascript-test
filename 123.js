059a64cd73d2828220b2a20ca02e5331a9b8ecc5
Maria Voroncova <marijagrigule@gmail.com>
2023-11-19 14:58:52 +0200

    initial commit

---
+++ 123.js
@@ -0,0 +1,16 @@
+console.log("hello")
+console.log("hello")
+console.log("asdasdhello")
+let abc = Number("123");
+
+console.log(abc)
+
+export default class AbcTrololo {}
+/*
+
+Ctrl+alt+L = format file
+Ctrl+alt+O = optimize imports
+Ctrl+shift+enter = finish statement
+Ctrl+shift+f10 = execute current file
+
+ */
