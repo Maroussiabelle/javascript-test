23e1822c8886ef2a9cb796323c608f94d8ca4db5
Maria Voroncova <marijagrigule@gmail.com>
2023-11-19 15:03:14 +0200

    updated 123.js

--- 123.js
+++ 123.js
@@ -4,13 +4,3 @@ console.log("asdasdhello")
 let abc = Number("123");
 
 console.log(abc)
-
-export default class AbcTrololo {}
-/*
-
-Ctrl+alt+L = format file
-Ctrl+alt+O = optimize imports
-Ctrl+shift+enter = finish statement
-Ctrl+shift+f10 = execute current file
-
- */
