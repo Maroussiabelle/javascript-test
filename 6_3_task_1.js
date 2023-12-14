44b28e9cebdeb7a691d6456195f5b90cbb8c5a33
Maria Voroncova <marijagrigule@gmail.com>
2023-12-14 22:03:07 +0200

    solution 5.4 task 2

--- 6_3_task_1.js
+++ 6_3_task_1.js
@@ -1,58 +1,54 @@
 
 /*
-Перед вами объект catalogue, в котором содержится свойство magazine: 5.
- Измените значение этого свойства на вводимое пользователем числовое значение
-  и выведите его, но только если оно не меньше 10 и не больше 50.
-  Если введенное значение находится вне данного диапазона,
-  сообщите об этом пользователю в заявленном формате.
+
+
+Создайте объект c единственным методом,
+который принимает два числа, введенных пользователем,
+ как параметры и выводит остаток от деления первого числа на второе.
 
 Sample Input 1:
 
-50
+5
+2
 
 Sample Output 1:
 
-50
+1
 
 Sample Input 2:
 
 9
+3
 
 Sample Output 2:
 
-Введите число в диапазоне от 10 до 50 включительно
+0
+
+Sample Input 3:
+
+7
+5
+
+Sample Output 3:
+
+2
+
+
 
 
  */
 
-
-const catalogue = {
-    magazine: 5,
+const res = {
+    displayResidual(num1, num2) {
+        console.log(num1 % num2);
+    }
 };
 
-//catalogue.magazine = Number(prompt("Введите значение для свойства magazine (число в диапазоне от 10 до 50 включительно):"));
-catalogue.magazine = 9;
-if (catalogue.magazine >= 10 && catalogue.magazine <= 50){
-    console.log(catalogue.magazine);
-} else{
-    console.log("Введите число в диапазоне от 10 до 50 включительно");
-}
+// let num1 = Number(prompt("Введите первое число"));
+// let num2 = Number(prompt("Введите второе число"));
 
+num1 = 5;
+num2 = 2;
 
-
-//let newspaper = Number(prompt());
-
-
-/*
-const catalogue = {
-    magazine: 5,
-}
-
-delete catalogue.magazine;
-
-catalogue.newspaper = Number(prompt("Введите значение для свойства newspaper:"));
-
-console.log(catalogue.newspaper);
-
-*/
+res.displayResidual(num1, num2);
 
