3e14e9c07474afa65d88efe1514b7b71c6837559
Maria Voroncova <marijagrigule@gmail.com>
2024-01-20 17:57:27 +0200

    solution 5.4 task 2

---
+++ 6_3_task_4.js
@@ -0,0 +1,32 @@
+let bankAccount = {
+    balance: 500,
+
+    deposit: function(amount) {
+       this.balance += amount;
+    },
+
+    withdraw: function(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+        } else {
+            console.log("Недостаточно средств на счете");
+        }
+    },
+};
+
+//const amount = Number(prompt());
+// const choice = prompt();
+const amount = 600;
+const choice = "снять";
+if (choice === "внести") {
+    bankAccount.deposit(amount);
+    console.log(bankAccount.balance);
+} else if (choice === "снять") {
+    if (amount <= bankAccount.balance) {
+       bankAccount.withdraw(amount);
+        console.log(bankAccount.balance);
+    } else {
+        console.log("Недостаточно средств на счете");
+
+    }
+}
