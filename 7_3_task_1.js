ca58cb96eadb29105f16313c2f3f0fc0fb344288
Maria Voroncova <marijagrigule@gmail.com>
2024-01-21 17:00:43 +0200

    solution 7.3 task 1

---
+++ 7_3_task_1.js
@@ -0,0 +1,13 @@
+// write a code that counts wowels in a String
+
+const text = "Истина парадоксальна";
+let vowelCount = 0;
+for (let i = 0; i < text.length; i++) {
+  let char = text[i].toLowerCase()
+  if (char === "а" || char === "е" || char === "и" || char === "о" || char === "у" || char === "э"
+|| char === "ю" || char === "я") {
+    vowelCount++;
+   }
+}
+
+console.log(vowelCount);
